7e2319e92c84a58196dbe4d03388afae293691cb
ANDRE Antoine <totoandre2507@gmail.com>
2025-03-14 14:37:53 +0100

    Update accueil.php

---
+++ pages/commun/accueil.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../style.css">
+
+</head>
+
+<body>
+
+    <div class="container">
+
+        <div class="Acceuil">
+
+            <h1>CongéFacile</h1>
+
+            <p>
+                CongéFacile est votre nouvel outil dédié à la gestion des congés au sein de l’entreprise. 
+                Plus besoin d’échanges interminables ou de formulaires papier : en quelques clics, vous pouvez gérer vos absences en toute transparence et simplicité.
+            </p>
+
+            <h2>Etapes</h2>
+
+            <div class="steps">
+                <div class="stepsCards">
+                    <div class="stepsCardsContainer"></div>
+                </div>
+            </div>
+
+
+
+
+
+        </div>
+    </div>
+
+
+</body>
+
+</html>
