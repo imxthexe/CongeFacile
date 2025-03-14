7e62a80a6978e8b1f68622715005a211d88a6a62
ANDRE Antoine <totoandre2507@gmail.com>
2025-03-14 14:43:56 +0100

    Update accueil.php

--- pages/commun/accueil.php
+++ pages/commun/accueil.php
@@ -18,17 +18,37 @@
             <h1>CongéFacile</h1>
 
             <p>
-                CongéFacile est votre nouvel outil dédié à la gestion des congés au sein de l’entreprise. 
+                CongéFacile est votre nouvel outil dédié à la gestion des congés au sein de l’entreprise.
                 Plus besoin d’échanges interminables ou de formulaires papier : en quelques clics, vous pouvez gérer vos absences en toute transparence et simplicité.
             </p>
 
             <h2>Etapes</h2>
 
             <div class="steps">
+
+            <div class="stepsCircle">
+            </div>
+
                 <div class="stepsCards">
-                    <div class="stepsCardsContainer"></div>
+                    <div class="stepsCardsContainer">
+                        <p>j’effectue ma demande de congés</p>
+                    </div>
+                </div>
+
+                <div class="stepsCards">
+                    <div class="stepsCardsContainer">
+                        <p>Mon manager valide ou refuse la demande</p>
+                    </div>
+                </div>
+
+                <div class="stepsCards">
+                    <div class="stepsCardsContainer">
+                        <p>Je consulte l’historique de mes demandes</p>
+                    </div>
                 </div>
             </div>
+
+            <p>En cas de difficulté avec l’application, veuillez envoyer un email à <u><a href="mailto:contact@mentalworks.fr">contact@mentalworks.fr.</a></u></p>
 
 
 
