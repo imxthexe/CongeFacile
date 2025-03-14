90be7c5692fe632ac6cd9054fd4a772254c69a75
Mathis Vosgien <mathis@vosgien.fr>
2025-03-14 13:53:24 +0100

    navbar

--- includes/navBar/navBar1.php
+++ includes/navBar/navBar1.php
@@ -1,8 +1,21 @@
-
 <link rel="stylesheet" href="style.css">
+
 <body>
-    <?php  include'header.php' ?>
     <div class="navBar">
 
+        <nav>
+            <ul>
+                <li><a href="">Accueil</a></li>
+                <li><a href="">Nouvelle demande</a></li>
+                <li><a href="">Historique des demandes</a></li>
+            </ul>
+            <hr>
+            <ul>
+                <li><a href="">Mes informations</a></li>
+                <li><a href="">Mes préférences</a></li>
+                <li><a href="">Deconnexion</a></li>
+            </ul>
+        </nav>
     </div>
+
 </body>
