1bc7fb178675198046df2ad792e5125b8409e31d
mxthis.exe <mathisvosgien@gmail.com>
2025-03-29 15:36:48 +0100

    responsive navBar

--- includes/navBar/navBar1.php
+++ includes/navBar/navBar1.php
@@ -1,40 +1,45 @@
 <link rel="stylesheet" href="../../style.css">
-<div class="navBar">
-    <div class="link">
-        <nav>
-            <ul>
-                <a href="../../pages/commun/accueil.php">
-                    <li>Accueil</li>
-                </a>
-                <a href="../../pages/collaborateur/nouvelleDemande.php">
-                    <li>Nouvelle demande</li>
-                </a>
-                <a href="../../pages/collaborateur/historiqueDesDemandes.php">
-                    <li>Historique des demandes</li>
-                </a>
-            </ul>
-            <hr>
-            <ul>
-                <a href="../../pages/collaborateur/mesInformation.php">
-                    <li>Mes informations</li>
-                </a>
-                <a href="../../pages/collaborateur/mesPreferences.php">
-                    <li>Mes préférences</li>
-                </a>
-                <a href="#">
-                    <li>Deconnexion</li>
-                </a>
-            </ul>
-        </nav>
-    </div>
-    <div class="profile">
-        <div class="img">
-            <img src="https://placehold.co/60x60" alt="profile">
-        </div>
-        <div>
-            <p class="name">Prénom Nom</p>
-            <p class="role">Role</p>
-        </div>
-    </div>
 
-</div>
+<body>
+    <input type="checkbox" id="checkbox">
+    <label for="checkbox" class="toggle">
+    </label>
+    <div class="navBar">
+        <div class="link">
+            <nav>
+                <ul>
+                    <a href="../../pages/commun/accueil.php">
+                        <li>Accueil</li>
+                    </a>
+                    <a href="../../pages/collaborateur/nouvelleDemande.php">
+                        <li>Nouvelle demande</li>
+                    </a>
+                    <a href="../../pages/collaborateur/historiqueDesDemandes.php">
+                        <li>Historique des demandes</li>
+                    </a>
+                </ul>
+                <hr>
+                <ul>
+                    <a href="../../pages/collaborateur/mesInformation.php">
+                        <li>Mes informations</li>
+                    </a>
+                    <a href="../../pages/collaborateur/mesPreferences.php">
+                        <li>Mes préférences</li>
+                    </a>
+                    <a href="#">
+                        <li>Deconnexion</li>
+                    </a>
+                </ul>
+            </nav>
+        </div>
+        <div class="profile">
+            <div class="img">
+                <img src="https://placehold.co/60x60" alt="profile">
+            </div>
+            <div>
+                <p class="name">Prénom Nom</p>
+                <p class="role">Role</p>
+            </div>
+        </div>
+    </div>
+</body>
