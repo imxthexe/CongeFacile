35033ba41d30be2818f825066760b47d90dc9db2
Mathis Vosgien <84449523+imxthexe@users.noreply.github.com>
2025-05-14 16:47:35 +0200

    modif nav

--- includes/navBar/navBar1.php
+++ includes/navBar/navBar1.php
@@ -51,13 +51,13 @@
             const links = document.querySelectorAll("a");
             const currentPage = window.location.href;
 
-
+            // Vérifier si un lien actif est stocké
             let activeLink = localStorage.getItem("activeLink");
 
-
+            // Supprimer les classes actives de tous les liens
             links.forEach(link => link.classList.remove("active-link"));
 
-
+            // Si un lien a été précédemment cliqué, on lui remet la classe active
             if (activeLink) {
                 links.forEach(link => {
                     if (link.href === activeLink) {
@@ -66,14 +66,14 @@
                 });
             }
 
-
+            // Ajouter la classe active au lien de la page actuelle
             links.forEach(link => {
                 if (link.href === currentPage) {
                     link.classList.add("active-link");
-                    localStorage.setItem("activeLink", link.href);
+                    localStorage.setItem("activeLink", link.href); // Mettre à jour le stockage
                 }
 
-                e
+                // Enregistrer le lien cliqué dans localStorage
                 link.addEventListener("click", function() {
                     localStorage.setItem("activeLink", this.href);
                 });
